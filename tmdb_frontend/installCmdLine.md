be0b34ca367cfc61cea78af83ef07baadac6749b
Luc Chan <luc92.chan@gmail.com>
2024-04-18 16:20:42 +0200

    feat: add header

---
+++ tmdb_frontend/installCmdLine.md
@@ -0,0 +1,12 @@
+<!-- vite -->
+https://vitejs.dev/guide/
+
+<!-- tailwind -->
+https://tailwindcss.com/docs/guides/vite
+
+<!-- import icone -->
+https://react-icons.github.io/react-icons/
+
+<!-- used icone locations-->
+https://react-icons.github.io/react-icons/icons/hi/
+https://react-icons.github.io/react-icons/icons/hi2/
---
+++ tmdb_frontend/src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import logo from './../assets/images/logo.png'
+import {
+	HiHome,
+	HiMagnifyingGlass,
+	HiStar,
+	HiPlayCircle,
+	HiTv,
+} from "react-icons/hi2"
+import {
+	HiPlus,
+	HiDotsVertical
+} from "react-icons/hi"
+import HeaderItem from './HeaderItem'
+
+function Header() {
+
+	const menu=[
+		{
+			name: 'HOME',
+			icon:HiHome
+		},
+		{
+			name: 'SEARCH',
+			icon:HiMagnifyingGlass
+		},
+		{
+			name: 'WATCHLIST',
+			icon:HiPlus
+		},
+		{
+			name: 'ORIGINALS',
+			icon:HiStar
+		},
+		{
+			name: 'MOVIES',
+			icon:HiPlayCircle
+		},
+		{
+			name: 'SERIES',
+			icon:HiTv
+		},
+	]
+
+  return (
+	<div className='flex items-center gap-8'>
+		<div className='flex gap-8 items-center'>
+			<img src={logo} className='w-[200px] md:w-[300px] object-cover'/>
+			{menu.map((item) => (
+				<HeaderItem name={item.name} Icon={item.icon} key={item.name}/>
+			))}
+		</div>
+	</div>
+  )
+}
+
+export default Header
