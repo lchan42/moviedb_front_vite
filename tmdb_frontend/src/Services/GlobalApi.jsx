a7c8553906f8f65c8500aabe485b2b7e2870896c
Luc Chan <luc92.chan@gmail.com>
2024-04-24 19:16:04 +0200

    feat: adding error management in Global api

--- tmdb_frontend/src/Services/GlobalApi.jsx
+++ tmdb_frontend/src/Services/GlobalApi.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 /************* URL ************ */
 const movieBaseUrl = "https://api.themoviedb.org/3"
@@ -12,11 +13,20 @@ const imgBaseUrl = "https://image.tmdb.org/t/p/original"
 
 
 /************* getters ************ */
-const getTrendingVideos = axios.get(trendingUrl)
-const getMovieGenreList = axios.get(urlMovieGenreList)
-const getTvShowGenreList = axios.get(urlTvShowGenreList)
-const getMovieByGenreId = (id) => axios.get(movieByGenreBaseURL+apiKey+"&with_genres="+id)
-const getMovieByName = (userInput) => axios.get(movieSearchBaseURL+apiKey+"&query="+userInput)
+const axiosGetRequest = (url) => axios.get(url).catch(e => handleError(e))
+
+const handleError = (e) => {
+  const navigate = useNavigate()
+
+  console.log(e)
+  navigate('/errorPage')
+}
+
+const getTrendingVideos = axiosGetRequest(trendingUrl)
+const getMovieGenreList = axiosGetRequest(urlMovieGenreList)
+const getTvShowGenreList = axiosGetRequest(urlTvShowGenreList)
+const getMovieByGenreId = (id) => axiosGetRequest(movieByGenreBaseURL+apiKey+"&with_genres="+id)
+const getMovieByName = (userInput) => axiosGetRequest(movieSearchBaseURL+apiKey+"&query="+userInput)
 
 export default {
 	getTrendingVideos,
@@ -26,27 +36,3 @@ export default {
   getMovieByName,
   imgBaseUrl,
 }
-
-
-// console.log("url moviegenre list "+urlMovieGenreList)
-// console.log("url TvShow genre list "+urlTvShowGenreList)
-
-// const trendingUrl = "https://api.themoviedb.org/3/trending/all/day?api_key=d38af7faa64af95a2676cd86fc7065e5"
-// const url = 'https://api.themoviedb.org/3/movie/11?api_key=d38af7faa64af95a2676cd86fc7065e5';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: import.meta.env.VITE_API_AUTH_BEARER
-//   }
-// };
-
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error('error:' + err));
-
-
-// const getTrendingVideos=axios.get(this.movieBaseUrl+"/trending/all/day?api_key" + apiKey)
-// const getMovieGenreList = axios.get(urlMovieGenreList).then((res) => console.log(res.data.genres))
